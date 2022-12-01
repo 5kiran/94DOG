a681b749bfa67af2e8b26a8144f2443878a940fd
OHGILHWAN <dhrlfghks01@gmail.com>
2022-12-01 20:10:51 +0900

    init : Start settings

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+  return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+  app.run('0.0.0.0', port=5000, debug=True)
