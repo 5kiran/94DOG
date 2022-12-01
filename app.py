a5e1c077c9e5e699ec10e456b41f813492c2c019
OHGILHWAN <dhrlfghks01@gmail.com>
2022-12-01 19:46:54 +0900

    init : Start Project

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
