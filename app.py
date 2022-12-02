b054a1faf5d2d8806e1c8811c2ad31aa34857796
OHGILHWAN <dhrlfghks01@gmail.com>
2022-12-03 05:24:40 +0900

    feat : 좋아요 기능 및 갯수 읽기 구현

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
+import pymysql
+
+db = pymysql.connect(host="localhost",
+                     port=3306,
+                     user="root",
+                     db='dog94',
+                     password='1qaz12',
+                     charset='utf8')
+
+curs = db.cursor()
 
 @app.route('/')
 def home():
   return render_template('index.html')
 
+@app.route('/liked')
+def liked():
+  return render_template('liked.html')
+
+@app.route('/liked',methods=['POST'])
+def like():
+  user_id = request.form['user_id_give']
+  board_id = request.form['board_id_give']
+  like_find = f'SELECT * FROM board LEFT JOIN liked ON board.id = liked.board_id WHERE board.id = {board_id} AND liked.user_id = {user_id}'
+  curs.execute(like_find)
+  result = curs.fetchone()
+  print(result)
+  if result is None:
+    like_up = f'INSERT INTO liked (user_id,board_id) VALUES ({user_id},{board_id})'
+    curs.execute(like_up)
+    db.commit()
+  else:
+    like_delete = f'DELETE FROM  liked WHERE liked.user_id = {user_id} AND liked.board_id = {board_id}'
+    print(like_delete)
+    curs.execute(like_delete)
+    db.commit()
+    
+  return jsonify({'msg': '좋아용'})
+
+@app.route("/liked/board", methods=["GET"])
+def board_like():
+  board_id = "SELECT id,title,content,file_url,user_id  FROM board WHERE id = 2"
+  curs.execute(board_id)
+  board_data = curs.fetchone()
+  
+  like_num = 'SELECT count(liked.board_id) FROM board LEFT JOIN liked ON board.id = liked.board_id WHERE board.id = 2'
+  curs.execute(like_num)
+  like_count = curs.fetchone()
+  print(like_count)
+  
+  like_board = [
+    like_count,
+    board_data
+  ]
+  
+  
+  return jsonify({'boardData': like_board})
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/JavaScript/liked.js
@@ -0,0 +1,41 @@
+function like(idNum){
+  const userId = document.getElementById('userId').value
+  const boardId = idNum
+  $.ajax({
+    type: "POST",
+    url: "/liked",
+    data: {user_id_give : userId,board_id_give : boardId},
+    success: function (response) {
+        alert(response["msg"])
+    }
+  });
+}
+function showBoard(){
+  $.ajax({
+    type: 'GET',
+    url: `/liked/board`,
+    data: {},
+    success: function (response) {
+      console.log(response['boardData'])
+      let id = response['boardData'][1][0]
+      let title = response['boardData'][1][1]
+      let content = response['boardData'][1][2]
+      let fileUrl = response['boardData'][1][3]
+      let userId = response['boardData'][1][4]
+      let likeCount = response['boardData'][0]
+
+      console.log(id,title,content,fileUrl,userId)
+
+      let temp_html = `<div id ="boardId${id}">아이디 : ${id}<br>
+                        제목 : ${title}<br>
+                        내용 : ${content}<br>
+                        파일 : ${fileUrl}<br>
+                        User_id : ${userId}<br>
+                        좋아요 카운트 : ${likeCount}
+                        <button onclick= "like(${id})" type="button" class="btn btn-dark">좋아요 </button>
+                      </div>`
+      $('#board').append(temp_html)
+    
+    }
+  });
+}
