d9bf82728855251d8a36fc4306b711065ac7bc16
Nomik94 <kjungmin0374@gmail.com>
2022-12-02 22:53:04 +0900

    feat:회원가입 미완성

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request, jsonify
+import pymysql
+
 app = Flask(__name__)
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-@app.route('/')
-def home():
-  return render_template('index.html')
+db = pymysql.connect(host="localhost",
+                      port=3306,
+                      user="root",
+                      db='dog94',
+                      password='Jungmin0413',
+                      charset='utf8')
 
+curs = db.cursor()
 
+@app.route("/register", methods=["POST"])
+def user_register():
+  params = request.get_json()
+  name = params['name']
+  psword = params['psword']
+  email = params['email']
+
+  sql = """insert into user (name, email, psword)
+            values (%s,%s,%s)
+        """
+  curs.execute(sql, (name, email, psword))
+  curs.fetchall()
+  db.commit()
+  print(params)
+  return "ok"
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug=True)
