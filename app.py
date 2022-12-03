1b43aabdb5ea8479d3dcc5191a04b05346340b55
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2022-12-04 01:21:49 +0900

    feat: 작업 환경 세팅

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def home():
   return render_template('index.html')
 
 
+@app.route('/pagination', methods=['GET'])
+def pagination():
+  data = {'name': 'test'}
+  return render_template('components/pagination.html', data=data)
+
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug=True)
