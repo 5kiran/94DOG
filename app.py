6acf8c4eb5c67a8e20408b15f25c053d432d1092
Joto-Han <ais726@naver.com>
2022-12-05 22:11:28 +0900

    게시판 작성,삭제,수정,출력

--- app.py
+++ app.py
@@ -1,11 +1,130 @@
 from flask import Flask, jsonify, render_template, request
+import pymysql
 app = Flask(__name__)
+
+# db가 아닌 다른 변수명으로 써도 됩니다.
+db = pymysql.connect(
+    host='127.0.0.1',
+    user='root',
+    db='dog94',
+    password='a123456',
+    charset='utf8')
+
+curs = db.cursor(pymysql.cursors.DictCursor)
+
+qurey = db.cursor()
 
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route("/temp")
+def profile():
+    return render_template("post.html")
+
+
+@app.route("/viewpost-layout")
+def viewpost():
+    return render_template("viewpost.html")
+
+
+# 게시글 저장 기능
+@app.route('/post', methods=['POST'])
+def save_post():
+
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+    date_receive =request.form.get('data_give')
+    # created_at_receive = request.form.get('created_at_give')
+    
+    # 이건 옛날 방식
+    # title_receive = request.form['title_give']
+    # content_receive = request.form['content_give']
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive,
+        'updated_at': date_receive,
+        # 'created_at': created_at_receive
+    }
+
+    curs.execute(
+        f"insert into board (title,content,updated_at) value ('{title_receive}','{content_receive}','{date_receive}')")
+    db.commit()
+    # 스트링 합 연산자
+
+    return jsonify({'msg': '게시글 저장 완료!'})
+
+
+# 게시글 수정 기능
+@app.route('/post/update', methods=['POST'])
+def update_post():
+
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+    id_receive = request.form.get('id_give')
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive,
+        'id': id_receive,
+    }
+
+    curs.execute(
+        f"update board set title='{title_receive}',content='{content_receive}' where id='{id_receive}'")
+    db.commit()
+    # 스트링 합 연산자
+
+    return jsonify({'msg': '게시글 수정 완료!'})
+
+
+# 게시글 삭제 기능
+@app.route('/post/delete', methods=['POST'])
+def delete_post():
+
+    id_receive = request.form.get('id_give')
+
+    doc = {
+        'id': id_receive,
+    }
+
+    curs.execute(
+        f"update board set deleted=1 where id='{id_receive}'")
+    db.commit()
+    # 스트링 합 연산자
+
+    return jsonify({'msg': '게시글 삭제 완료!'})
+
+
+# 게시글 보기 기능
+@app.route('/post', methods=['get'])
+def show_post():
+
+    # sql = "select title,content from board"
+    sql = "select * from board" 
+
+    qurey.execute(sql)
+
+    people = qurey.fetchall()
+
+    return jsonify({'show_post':people})
+
+
+# 게시글 타이틀(링크)를 클릭하면 해당 페이지로 이동하는 기능
+# 예를 들어 4번 게시글을 클릭하면 4번 게시글의 정보를 받아와서 페이지로 이동
+@app.route('/views/<id>', methods=['get'])
+def view_post(id):
+
+    curs.execute(
+        f"select * from board where id='{id}'")
+
+    view_post = curs.fetchall()
+
+    db.commit()
+    return jsonify({'view_post_list':view_post})
 
 
 if __name__ == '__main__':
-  app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5500, debug=True)
---
+++ static/JavaScript/post.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+    show_post()
+})
+
+function getFormatDate(date){
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth());
+    month = month > 10 ? month : '0' + month; // 10이 넘지 않으면 앞에 0을 붙인다
+    var day = date.getDate();
+    day = day > 10 ? day : '0' + day; // 10이 넘지 않으면 앞에 0을 붙인다
+    var hours = date.getHours();
+    hours = hours > 10 ? hours : '0' + hours; // 10이 넘지 않으면 앞에 0을 붙인다
+    var minutes = date.getMinutes();
+    minutes =  minutes > 10 ? minutes : '0' + minutes; // 10이 넘지 않으면 앞에 0을 붙인다
+    var seconds = date.getSeconds();
+    seconds = seconds > 10 ? seconds : '0' + seconds; // 10이 넘지 않으면 앞에 0을 붙인다
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
+const date = getFormatDate(new Date()); // 오늘 날짜 지정
+console.log('현재시간은 :' + date)
+// console.log('현재시각 :' + new_date)
+
+function save_post() {
+    const title = $('#title').val();
+    const content = $('#content').val();
+
+    if (title === '' || content === '') {
+        alert('빈칸을 모두 채워주세요 T^T')
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/post",
+            data: {
+                title_give: title,
+                content_give: content,
+                data_give: date
+            },
+            success: function (response) {
+                alert(response['msg'])
+                window.location.reload()
+            }
+        });
+    }
+}
+
+function update_post() {
+    const title = $('#title').val();
+    const content = $('#content').val();
+    const id = $('#id').val();
+
+    if (id === '') {
+        alert('id 값은 빈칸일 수 없어요')
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/post/update",
+            data: {
+                title_give: title,
+                content_give: content,
+                id_give: id
+            },
+            success: function (response) {
+                alert(response['msg'])
+                window.location.reload()
+            }
+        });
+    }
+}
+
+function delete_post() {
+    const id = $('#id').val();
+    if (id === '') {
+        alert('id 값은 빈칸일 수 없어요')
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/post/delete",
+            data: { id_give: id },
+            success: function (response) {
+                alert(response['msg'])
+                window.location.reload()
+            }
+        });
+    }
+}
+function show_post() {
+    $('#card').empty()
+    $.ajax({
+        type: "get",
+        url: "/post",
+        data: {},
+        success: function (response) {
+            let rows = response['show_post']
+
+            for (let i = 0; i < rows.length; i++) {
+                let title = rows[i][1]
+                let id = rows[i][0]
+                let content = rows[i][2]
+                let deleted = rows[i][5]
+                let update_at = rows[i][4]
+                let temp_html = ``
+                if (deleted == 0) {
+                    temp_html = `<div class="card-header"><a href="/viewpost-layout?id=${id}">
+                                    ${title}</a>
+                                </div>
+                                <div class="card-body">                            
+                                    <p class="card-text">${content} , ${update_at}</p>`
+                }
+
+                $('#card').append(temp_html)
+
+            }
+        }
+    });
+}
+
+function cancel_post() {
+    alert('게시글 쓰기에서 나가는 버튼! 아직은 미구현...')
+}
+
+
