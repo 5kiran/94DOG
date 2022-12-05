276a1aac3047c280f697b7a22fcb0a2f9c4a1ad2
OHGILHWAN <dhrlfghks01@gmail.com>
2022-12-05 12:02:18 +0900

    test : 테스트 코드 추가

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def like():
   like_find = f'SELECT * FROM board LEFT JOIN liked ON board.id = liked.board_id WHERE board.id = {board_id} AND liked.user_id = {user_id}'
   curs.execute(like_find)
   result = curs.fetchone()
-  print(result)
+  
   
   if result is None:
     like_up = f'INSERT INTO liked (user_id,board_id,writer_id) VALUES ({user_id},{board_id},{writer_id})'
@@ -36,6 +36,7 @@ def like():
     curs.execute(like_up)
     curs.execute(board_like_up)
     db.commit()
+    
   else:
     like_delete = f'DELETE FROM  liked WHERE liked.user_id = {user_id} AND liked.board_id = {board_id}'
     board_like_down = f'UPDATE board SET liked = liked -1 WHERE board.id = {board_id}'
@@ -47,19 +48,18 @@ def like():
 
 @app.route("/liked/board", methods=["GET"])
 def board_like():
-  board_id = "SELECT id,title,content,file_url,user_id,liked  FROM board WHERE id = 6"
+  board_id = "SELECT id,title,content,file_url,user_id,liked  FROM board WHERE id = 1"
   curs.execute(board_id)
-  board_data = curs.fetchone()
-  print(board_data)  
+  board_data = curs.fetchone()  
   
   return jsonify({'boardData': board_data})
 
 @app.route("/liked/rank", methods=["GET"])
 def like_rank():
-  sql = 'SELECT `user`.name,count(writer_id) AS like_cnt FROM liked LEFT JOIN `user` ON liked.writer_id = `user`.id GROUP BY `user`.name ORDER BY  like_cnt DESC LIMIT 5'
+  sql = 'SELECT `user`.name,count(writer_id) AS like_cnt FROM liked LEFT JOIN `user`ON liked.writer_id = `user`.id GROUP BY `user`.name ORDER BY  like_cnt DESC LIMIT 5'
   curs.execute(sql)
   like_data = curs.fetchall()
-  print(like_data)
+  
   return jsonify({'likeRankList' :like_data})
 
 
--- static/JavaScript/likeranking.js
+++ static/JavaScript/likeranking.js
@@ -5,15 +5,15 @@ function showRank(){
     data: {},
     success: function (response) {
       let rows = response['likeRankList']
-
+      let count = 0
       for(let i in rows){
         let name = rows[i]['name']
         let rank = rows[i]['like_cnt']
         console.log(name,rank)
-
+        count += 1
         let temp_html = `
                           <div id = "likeRank" >
-                            <div>${i}위</div>
+                            <div>${count}위</div>
                             <div>${name}</div>
                             <div>${rank}개</div>
                           `
